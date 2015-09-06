722c4f3de7669a1219353d8e03833bd31cfbcd9c
Ian Liao <ian110652@gmail.com>
2015-09-06 13:16:28 +0800

    使用 css 與 style 載入器動態載入 style.css 檔案，並在 CSS 轉換格式時自動最小化

--- demo02/main2.js
+++ demo02/main2.js
@@ -1,1 +1,11 @@
+var $ = require('jquery');
+
 document.write('<h2>Hello Webpack</h2>');
+
+// $(function () {
+	
+$('h2').click(function(){
+	$(this).html('main2 jquery test..');
+	require('./style.css');
+});
+// })

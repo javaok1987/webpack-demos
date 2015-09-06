232132fe3e12cc573d41b29046aad2ba07921ede
Ian Liao <ian110652@gmail.com>
2015-09-06 22:21:50 +0800

    使用 CommonsChunkPlugin 外掛

---
+++ demo11/bundle1.js
@@ -0,0 +1,15 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+	React.render(React.createElement(
+	  'h1',
+	  null,
+	  'Hello World'
+	), document.getElementById('a'));
+
+/***/ }
+]);
---
+++ demo11/bundle2.js
@@ -0,0 +1,15 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+	React.render(React.createElement(
+	  'h2',
+	  null,
+	  'Hello Webpack'
+	), document.getElementById('b'));
+
+/***/ }
+]);

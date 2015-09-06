01c8961a1f0d993aa6092c5e530aaf269ebf294f
Ian Liao <ian110652@gmail.com>
2015-09-06 11:01:22 +0800

    載入 jquery 模組，在 main1.js 中透過 jQuery 註冊一個 button 事件載入 main2.js

--- demo02/main1.js
+++ demo02/main1.js
@@ -1,9 +1,13 @@
+var $ = require('jquery');
+
 document.write('<h1>Hello World</h1>');
 
-function load_main2 (argument) {
-	// body...
+// function load_main2 (argument) {
+	// require('./main2.js');
+// }
+// window.load_main2 = load_main2;
+
+$('button').click(function(){
 	require('./main2.js');
-}
+});
 
-
-window.load_main2 = load_main2;
